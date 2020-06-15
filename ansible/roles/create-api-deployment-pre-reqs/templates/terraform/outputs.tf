b84e71e2d62346027052d4fcb7b3362763a05cc5
matt.mercer <matt.mercer@nhs.net>
2020-06-15 22:34:51 +0100

    apm-1017: create api deployment pre-reqs

---
+++ ansible/roles/create-api-deployment-pre-reqs/templates/terraform/outputs.tf
@@ -0,0 +1,20 @@
+
+output "vpc_id" {
+  value = data.terraform_remote_state.account.outputs.account_vpc.id
+}
+
+output "public_alb_listener_arn" {
+  value = local.public_alb_listener.arn
+}
+
+output "subnet_ids" {
+  value = data.terraform_remote_state.account.outputs.apis.subnet_ids
+}
+
+output "ecs_cluster_id" {
+  value = local.ecs_cluster.id
+}
+
+output "ecs_sg_id" {
+  value = data.terraform_remote_state.account.outputs.apis.sg.apis-ecs-cluster[var.apigee_environment].id
+}
