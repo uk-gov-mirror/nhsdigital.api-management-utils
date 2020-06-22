1be64dbea2d492ddddf9ab505af820d709c96e1d
matt.mercer <matt.mercer@nhs.net>
2020-06-22 17:06:20 +0100

    apm-1017: env service name should be from locals

--- ansible/roles/create-api-deployment-pre-reqs/templates/terraform/sg.tf
+++ ansible/roles/create-api-deployment-pre-reqs/templates/terraform/sg.tf
@@ -2,11 +2,11 @@
 resource "aws_security_group" "api-deployment" {
 
   vpc_id      = data.terraform_remote_state.account.outputs.account_vpc.id
-  name        = "api-${env_service_name}"
-  description = "api-${env_service_name} specific rules"
+  name        = "api-${local.env_service_name}"
+  description = "api-${local.env_service_name} specific rules"
 
   tags = {
-    Name   = "api-${env_service_name}"
+    Name   = "api-${local.env_service_name}"
     source = "terraform"
   }
 }
