a302164abce3b69781d3517b590a6baf71d8eeca
matt.mercer <matt.mercer@nhs.net>
2020-08-27 14:35:25 +0100

    apm-1266: allow pr deploys of ECS too

--- ansible/roles/create-api-deployment-pre-reqs/templates/terraform/locals.tf
+++ ansible/roles/create-api-deployment-pre-reqs/templates/terraform/locals.tf
@@ -7,10 +7,10 @@ locals {
   aws_ecr_bucket = "prod-eu-west-2-starport-layer-bucket"
 
   env_service_id       = "${var.apigee_environment}-${var.service_id}"
-  service_namespaces     = var.apigee_environment == "internal-dev" ? [var.service_id, "${var.service_id}-*"] : [var.service_id]
+  service_namespaces     = contains(["internal-dev", "internal-dev-sandbox"], var.apigee_environment) ? [var.service_id, "${var.service_id}-*"] : [var.service_id]
   env_service_namespaces = [for ns in local.service_namespaces : "${var.apigee_environment}-${ns}"]
   short_env_service_namespaces = [for ns in local.service_namespaces : "${var.apigee_shortenv}-${ns}"]
-  workspaces             = var.apigee_environment == "internal-dev" ? [var.workspace, "${var.workspace}:*"] : [var.workspace]
+  workspaces             = contains(["internal-dev", "internal-dev-sandbox"], var.apigee_environment) ? [var.workspace, "${var.workspace}:*"] : [var.workspace]
 
 
   public_alb_listener = data.terraform_remote_state.account.outputs.apis.alb-listener.public[var.apigee_environment]
