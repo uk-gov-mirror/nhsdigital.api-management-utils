6805438faacc3df918f3c338592e05abab79ccc9
Ben Strutt <ben.strutt1@nhs.net>
2021-01-11 12:19:01 +0000

    APM-1646 Hotfix 3

--- ansible/collections/ansible_collections/nhsd/apigee/plugins/module_utils/models/ansible/deploy_api_catalog_item.py
+++ ansible/collections/ansible_collections/nhsd/apigee/plugins/module_utils/models/ansible/deploy_api_catalog_item.py
@@ -3,6 +3,6 @@ from ansible_collections.nhsd.apigee.plugins.module_utils.models.apigee.apidoc i
 
 
 class DeployApidoc(pydantic.BaseModel):
-    catalog_item: ApigeeApidoc
+    api_catalog_item: ApigeeApidoc
     organization: str
     access_token: str
