7296f7333b87c9757fe0a9f99478ada4d4e5d7e8
Ben Strutt <ben.strutt1@nhs.net>
2021-01-12 13:49:16 +0000

    APM-0000 Attempt fix for broken Apigee API

--- ansible/collections/ansible_collections/nhsd/apigee/plugins/action/deploy_spec.py
+++ ansible/collections/ansible_collections/nhsd/apigee/plugins/action/deploy_spec.py
@@ -1,16 +1,17 @@
+import json
 from ansible_collections.nhsd.apigee.plugins.module_utils.models.ansible.deploy_spec import (
     DeploySpec,
 )
 from ansible_collections.nhsd.apigee.plugins.module_utils.apigee_action import (
     ApigeeAction,
 )
+from ansible_collections.nhsd.apigee.plugins.module_utils import utils
+from ansible_collections.nhsd.apigee.plugins.module_utils import constants
 
-APIGEE_DAPI_URL = "https://apigee.com/dapi/api/"
 
 
 class ActionModule(ApigeeAction):
     def run(self, tmp=None, task_vars=None):
-
         super(ActionModule, self).run(tmp, task_vars)
         args, errors = self.validate_args(DeploySpec)
         if errors:
@@ -19,46 +20,26 @@ class ActionModule(ApigeeAction):
         diff_mode = self._play_context.diff
         check_mode = self._play_context.check_mode
 
-        result = {"changed": False}
+        task_vars = utils.get_all_spec_resources(args.organization, args.access_token, task_vars=task_vars)
 
-        # Ensure we have the facts APIGEE_SPEC_RESOURCES and APIGEE_SPEC_FOLDER_ID
-        if not task_vars.get("APIGEE_SPEC_RESOURCES") or not task_vars.get(
-            "APIGEE_SPEC_FOLDER_ID"
-        ):
-            specs_list_uri = (
-                APIGEE_DAPI_URL
-                + f"/organizations/{args.organization}/specs/folder/home"
-            )
-
-            all_specs_response = self.get(specs_list_uri, args.access_token)
-
-            all_specs = all_specs_response["response"]["body"]
-            if all_specs.get("failed"):
-                return all_specs
-
-            task_vars["APIGEE_SPEC_RESOURCES"] = all_specs["contents"]
-            task_vars["APIGEE_SPEC_FOLDER_ID"] = all_specs["id"]
-
-            # Set them as facts for future calls in the same playbook
-            result["ansible_facts"] = {
+        result = {
+            "ansible_facts": {
                 "APIGEE_SPEC_RESOURCES": task_vars["APIGEE_SPEC_RESOURCES"],
                 "APIGEE_SPEC_FOLDER_ID": task_vars["APIGEE_SPEC_FOLDER_ID"],
             }
+        }
 
-        existing_resources = [
-            existing_resource
-            for existing_resource in task_vars.get(
-                "APIGEE_SPEC_RESOURCES",
-                result["ansible_facts"]["APIGEE_SPEC_RESOURCES"],
+        try:
+            existing_resources = utils.select_unique(
+                task_vars["APIGEE_SPEC_RESOURCES"],
+                "name",
+                args.spec.name,
             )
-            if existing_resource["name"] == args.spec.name
-        ]
-
-        if len(existing_resources) > 1:
+        except ValueError as e:
             return {
                 "failed": True,
-                "msg": f"Found more than one spec with name {args.spec.name}, this should not happen",
-                "existing_resources": existing_resources,
+                "msg": f"Could not find unique spec resouce with name {args.spec.name}",
+                "existing_resources": json.loads(str(e)),
             }
 
         if len(existing_resources) == 0:
@@ -68,18 +49,17 @@ class ActionModule(ApigeeAction):
                 "kind": "Doc",
             }
 
-            response_dict = self.post(
-                APIGEE_DAPI_URL + f"organizations/{args.organization}/specs/doc",
-                args.access_token,
-                json=spec_resource,
-            )
-            if response_dict.get("failed"):
-                return response_dict
+            if not check_mode:
+                response_dict = utils.post(
+                    constants.APIGEE_DAPI_URL + f"organizations/{args.organization}/specs/doc",
+                    args.access_token,
+                    json=spec_resource,
+                )
+                if response_dict.get("failed"):
+                    return response_dict
+                spec_resource = response_dict["response"]["body"]
 
-            spec_resource = response_dict["response"]["body"]
-            result["changed"] = True
-
-            # Add new spec to locally copy of all spec_resources
+            # add new spec to locally copy of all spec_resources
             task_vars["APIGEE_SPEC_RESOURCES"].insert(0, spec_resource)
             result["ansible_facts"]["APIGEE_SPEC_RESOURCES"] = task_vars[
                 "APIGEE_SPEC_RESOURCES"
@@ -89,28 +69,32 @@ class ActionModule(ApigeeAction):
 
         result["spec_resource"] = spec_resource
 
-        # Get current spec content
-        spec_content_url = (
-            APIGEE_DAPI_URL
-            + f"organizations/{args.organization}/specs/doc/{spec_resource['id']}/content"
-        )
-        current_spec_request = self.get(
-            spec_content_url, args.access_token, status_code=[200, 204]
-        )  # returns 204 for no content
-        if current_spec_request.get("failed"):
-            return current_spec_request
+        if 'id' in spec_resource:
+            # Get current spec content
+            spec_content_url = (
+                constants.APIGEE_DAPI_URL
+                + f"organizations/{args.organization}/specs/doc/{spec_resource['id']}/content"
+            )
+            current_spec_request = utils.get(
+                spec_content_url, args.access_token, status_code=[200, 204]
+            )  # returns 204 for no content
+            if current_spec_request.get("failed"):
+                return current_spec_request
 
-        spec_content = current_spec_request["response"]["body"]
+            current_spec_content = current_spec_request["response"]["body"]
+        else:
+            current_spec_content = None
+
         new_spec_content = args.spec.content
 
-        delta = self.delta(spec_content, new_spec_content)
+        delta = utils.delta(current_spec_content, new_spec_content)
         result["changed"] = bool(delta)
 
         if diff_mode:
             result["diff"] = [
                 {
                     "before_header": args.spec.name,
-                    "before": spec_content,
+                    "before": current_spec_content,
                     "after_header": args.spec.name,
                     "after": new_spec_content,
                 }
@@ -120,12 +104,12 @@ class ActionModule(ApigeeAction):
             result["spec_content"] = args.spec.content
             return result
 
-        new_spec_content_request = self.put(
+        new_spec_content_request = utils.put(
             spec_content_url, args.access_token, json=new_spec_content
         )
         if new_spec_content_request.get("failed"):
             return new_spec_content_request
-        self.put(spec_content_url + "/snapshot", args.access_token)
+        utils.put(spec_content_url + "/snapshot", args.access_token)
 
         result["spec_content"] = new_spec_content_request["response"]["body"]
         if diff_mode:
