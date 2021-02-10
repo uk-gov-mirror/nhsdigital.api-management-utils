03ba164a85cd52557813eeb2354d132204cee393
Henry Iwanejko <henry.iwanejko1@nhs.net>
2021-02-10 12:21:36 +0000

    APM-1216 split up script to seperate class

---
+++ scripts/test_pull_request_deployments.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from multiprocessing import Process
+from trigger_pipelines import AzureDevOps
+
+UTILS_PR_NUMBER = os.environ["UTILS_PR_NUMBER"]
+
+PULL_REQUEST_PIPELINES = {
+    "identity-service": {
+        "build": 27,
+        "pr": 54,
+        "branch": "refs/heads/master"
+    },
+    # "canary-api": {
+    #     "build": 222,
+    #     "pr": 223,
+    #     "branch": "refs/heads/main"
+    # },
+    # "personal-demographics-service": {
+    #     "build": 140,
+    #     "pr": 144,
+    #     "branch": "refs/heads/master"
+    # }
+}
+
+
+def trigger_pipelines(pipeline_ids: dict, service: str):
+    azure_dev_ops = AzureDevOps(utils_pr_number=UTILS_PR_NUMBER)
+    build_status = azure_dev_ops.run_pipeline(
+        service=service,
+        pipeline_type="build",
+        pipeline_id=pipeline_ids["build"],
+        pipeline_branch=pipeline_ids["branch"],
+        wait_for_completion=True
+    )
+    if build_status != "succeeded":
+        return
+    azure_dev_ops.run_pipeline(
+        service=service,
+        pipeline_type="pr",
+        pipeline_id=pipeline_ids["pr"],
+        pipeline_branch=pipeline_ids["branch"],
+        wait_for_completion=True
+    )
+
+
+def main():
+    jobs = []
+    for service, pipeline_ids in PULL_REQUEST_PIPELINES.items():
+        process = Process(target=trigger_pipelines, args=(pipeline_ids, service,))
+        process.start()
+        jobs.append(process)
+    for process in jobs:
+        process.join()
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/trigger_pipelines.py
@@ -0,0 +1,87 @@
+import os
+import json
+import time
+import requests
+
+
+class AzureDevOps:
+
+    def __init__(self, utils_pr_number: int = None):
+        self.base_url = "https://dev.azure.com/NHSD-APIM/API Platform/_apis/pipelines"
+        self.token = os.environ["AZURE_TOKEN"]
+        self.auth = requests.auth.HTTPBasicAuth("", self.token)
+        self.branch_name = os.environ["BRANCH_NAME"]
+        self.notify_commit_sha = os.environ["NOTIFY_COMMIT_SHA"]
+        self.utils_pr_number = utils_pr_number
+        self.notify_github_repo = "NHSDigital/api-management-utils"
+        self.api_params = {"api-version": "6.0-preview.1"}
+        self.api_request_delay = 60
+
+    @staticmethod
+    def print_response(response: requests.Response, note: str, verbose: bool = True) -> None:
+        if verbose:
+            print(note)
+            try:
+                print(json.dumps(response.json(), indent=2))
+            except json.decoder.JSONDecodeError:
+                print(response.content.decode())
+
+    def run_pipeline(self,
+                     service: str,
+                     pipeline_type: str,
+                     pipeline_id: int,
+                     pipeline_branch: str,
+                     wait_for_completion: bool = False) -> int:
+
+        run_url = self.base_url + f"/{pipeline_id}/runs"
+        body = {
+            "resources": {
+                "repositories": {
+                    "common": {
+                        "repository": {
+                            "fullName": "NHSDigital/api-management-utils",
+                            "type": "gitHub",
+                        },
+                        "refName": f"refs/pull/{self.utils_pr_number}/merge",
+                    },
+                    "self": {"refName": f"{pipeline_branch}"},
+                }
+            },
+            "variables": {
+                "NOTIFY_GITHUB_REPOSITORY": {
+                    "isSecret  ": False,
+                    "value": f"{self.notify_github_repo}",
+                },
+                "NOTIFY_COMMIT_SHA": {
+                    "isSecret  ": False,
+                    "value": f"{self.notify_commit_sha}"
+                },
+            },
+        }
+
+        if self.utils_pr_number is not None:
+            body["variables"]["UTILS_PR_NUMBER"] = {
+                "isSecret  ": False,
+                "value": f"{self.utils_pr_number}",
+            }
+
+        response = requests.post(run_url, auth=self.auth, params=self.api_params, json=body)
+        self.print_response(response, f"Initial request to {run_url}")
+
+        result = "failed"
+        if wait_for_completion and response.status_code == 200:
+            delay = 0
+            state_url = response.json()["_links"]["self"]["href"]
+            while response.status_code == 200 and response.json()["state"] == "inProgress":
+                time.sleep(self.api_request_delay)
+                delay = delay + self.api_request_delay
+                response = requests.get(state_url, auth=self.auth, params=self.api_params)
+                self.print_response(response, f"Response from {state_url} after {delay} seconds")
+            result = response.json()["result"]
+            print(f"Result of {service} {pipeline_type} pipeline: {result}")
+        elif response.status_code == 203 or response.status_code == 401:
+            print(f"{response.status_code}: Invalid or expired PAT (Personal Access Token),"
+                  f" please verify or renew token")
+        else:
+            print(f"Triggering pipeline: {service} {pipeline_type} failed, status code: {response.status_code}")
+        return result
